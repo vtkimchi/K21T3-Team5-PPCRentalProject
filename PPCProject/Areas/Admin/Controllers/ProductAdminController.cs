438491f1d85084128634eb1844dc65875757fa94
hanhphucao272 <hanhphucao1>
2017-11-20 19:34:07 +0700

    Phần Edit

---
+++ PPCProject/Areas/Admin/Controllers/ProductAdminController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCProject.Model;
+
+namespace PPCProject.Areas.Admin.Controllers
+{
+    public class ProductAdminController : Controller
+    {
+        //
+        // GET: /Admin/ProductAdmin/
+        Team35Entities model = new Team35Entities();
+        public ActionResult Index()
+        {
+            var product = model.PROPERTies.OrderByDescending(x => x.ID).ToList();
+            return View(product);
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var product = model.PROPERTies.FirstOrDefault(x => x.ID == id);
+            ViewBag.property_type = model.PROPERTY_TYPE.OrderByDescending(x => x.ID).ToList();
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, PROPERTY p)
+        {
+            var product = model.PROPERTies.FirstOrDefault(x => x.ID == id);
+            product.PropertyName = p.PropertyName;
+            product.Price = p.Price;
+            product.Content = p.Content;
+            product.Area = p.Area;
+            product.BedRoom = p.BedRoom;
+            product.BathRoom = p.BathRoom;
+            product.Created_at = p.Created_at;
+            product.Create_post = p.Create_post;
+            product.Updated_at = p.Updated_at;
+            model.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(PROPERTY p)
+        {
+            var product = new PROPERTY();
+            product.PropertyName = p.PropertyName;
+            product.Price = p.Price;
+            product.Content = p.Content;
+            product.Area = p.Area;
+            product.BedRoom = p.BedRoom;
+            product.BathRoom = p.BathRoom;
+            product.Created_at = p.Created_at;
+            product.Create_post = p.Create_post;
+            product.Updated_at = p.Updated_at;
+            model.PROPERTies.Add(product);
+            model.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id) 
+        {
+            var product = model.PROPERTies.FirstOrDefault(x => x.ID == id);
+            return View(product);
+        }
+        [HttpPost]
+        [ActionName("Delete")] 
+        public ActionResult DeleteConfirm(int id)
+        {
+            var product = model.PROPERTies.FirstOrDefault(x => x.ID == id);
+            model.PROPERTies.Remove(product);
+            model.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            var product = model.PROPERTies.FirstOrDefault(x => x.ID == id);
+            return View(product);
+        }
+	}
+}
