a86944e16176b014e9cbf6186107284716f54d1e
lethanhson2910 <lethanhson2910@gmail.com>
2017-12-18 18:39:22 +0700

    Change Password Funtional

---
+++ PPCProject/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCProject.Model;
+using System.IO;
+using PPCProject.Code;
+
+namespace PPCProject.Controllers
+{
+    public class UserController : Controller
+    {
+        team35Entities model = new team35Entities();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ChangePassword()
+        {
+            int id = (int)Session["UserID"];
+            var userdetail = model.USERs.FirstOrDefault(x => x.ID == id);
+            return View(userdetail);
+        }
+        [HttpPost]
+        public ActionResult ChangePassword(string currentpassword, string newpassword, string confirmnewpassword)
+        {
+            int id = (int)Session["UserID"];
+            var userdetail = model.USERs.FirstOrDefault(x => x.ID == id);
+            if (currentpassword == userdetail.Password)
+            {
+                if (confirmnewpassword == newpassword)
+                {
+                    userdetail.Password = newpassword;
+                    model.SaveChanges();
+                    TempData["changepassword"] = "Your password has been changed";
+                    return RedirectToAction("Index", "Home", new { area = "" });
+                }
+                else
+                {
+                    ModelState.AddModelError("confirmerror", "ConfirmPassword is wrong");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("passworderror", "Password is wrong");
+            }
+            return View();
+        }
+    }
+}
