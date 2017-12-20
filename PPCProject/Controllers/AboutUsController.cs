8aeb0a264d29c19c14d85bdc4e4f5171fd998e48
Dell <Dell@DESKTOP-CVL2LHM>
2017-12-20 21:43:33 +0700

    UpDate AboutUs

---
+++ PPCProject/Controllers/AboutUsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PPCProject.Model;
+
+namespace PPCProject.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        team35Entities model = new team35Entities();
+        // GET: AboutUs
+        public ActionResult AboutUs()
+        {
+            var AboutUs = model.ABOUT_US.FirstOrDefault(x => x.ID == 31);
+
+            return View(AboutUs);
+        }
+    }
+}
